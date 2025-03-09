32889725a7c75cfa8cc1cb9f9cf4fa981297ae42
jdmantillaq <jdmantillaq@unal.edu.co>
2025-03-09 10:03:59 -0500

    ENSO data procesing

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ENSOconomics: Unveiling the Economic Impact of El Niño and La Niña
 Investigate the relationship between ENSO (El Niño–Southern Oscillation) events and economic performance (GDP data) with a focus on sectors such as energy markets.
+
+
+## What is ENSO and Why is it Important?
+ENSO (El Niño–Southern Oscillation) is a recurring climate pattern characterized by fluctuations in sea surface temperatures (SSTs) in the central and eastern tropical Pacific Ocean. It has two primary phases:
+
+* **El Niño:** Warmer-than-average SSTs, often leading to extreme weather events like droughts, floods, and hurricanes.
+* **La Niña:** Cooler-than-average SSTs, typically causing opposite weather patterns to El Niño.
+
+## Why is ENSO Important Globally and Economically?
+ENSO significantly influences global weather, affecting agriculture, energy markets, infrastructure, and overall economic stability.
+
+- **Agriculture:** ENSO-driven droughts or excessive rainfall impact crop yields, food prices, and trade.
+- **Energy Markets:** Changes in temperature and precipitation affect electricity demand, hydropower generation, and fuel prices.
+- **Infrastructure & Trade:** Flooding and storms can damage infrastructure, disrupt supply chains, and slow economic growth.
+
+## Monitoring ENSO with the ONI Index
+The Oceanic Niño Index (ONI) is the primary measure for identifying ENSO events. It tracks SST anomalies in the Niño 3.4 region (5°N-5°S, 120°W-170°W).
+
+- ONI values ≥ **0.5°C** for five consecutive overlapping three-month periods indicate El Niño.
+- ONI values ≤ **-0.5°C** for the same duration indicate La Niña.
+
+
+<p align="center">
+    <img src="figures/oni_index.png" width="500px">
+</p>
---
+++ code/utilities.py
@@ -0,0 +1,81 @@
+def create_path(path):
+    """
+    Create a directory at the specified path if it does not already exist.
+
+    Parameters:
+    - path (str): The path of the directory to be created.
+    """
+    import os
+    os.makedirs(path, exist_ok=True)
+
+
+def get_files_in_folder(path, pattern):
+    """
+    Get a sorted array of file paths matching the specified pattern
+    within a folder.
+
+    Parameters:
+    - path (str): The path of the folder containing the files.
+    - pattern (str): The pattern to match the filenames.
+
+    Returns:
+    - numpy.ndarray: A sorted array of file paths.
+    """
+    import os
+    import numpy as np
+    import glob
+    return np.sort(glob.glob(os.path.join(path, pattern)))
+
+
+def walk_path_target(path, target):
+    """
+    Walk through the directory path and find file(s) matching the
+    specified target.
+
+    Parameters:
+    - path (str): The path of the directory to search.
+    - target (str): The target file(s) to search for.
+
+    Returns:
+    - numpy.ndarray: A sorted array of file paths matching the target.
+    """
+    import os
+    import numpy as np
+    import glob
+    # function to walk through the directory path and find target file(s)
+    return np.sort([y for x in os.walk(path) for y
+                    in glob.glob(os.path.join(x[0], target))])
+
+
+def file_in_folder(folder_path, file_name):
+    """
+    Check if a file exists within a folder.
+
+    Parameters:
+        folder_path (str or Path): Path to the folder where to check for the file.
+        file_name (str): Name of the file to check.
+
+    Returns:
+        bool: True if the file exists, False otherwise.
+    """
+    from pathlib import Path
+    folder = Path(folder_path)
+    file_path = folder / file_name
+    return file_path.exists()
+
+
+def font_properties():
+    from matplotlib import font_manager
+    ruta_fuentes = '../fonts/'
+
+    proptitle = font_manager.FontProperties(
+        fname=f'{ruta_fuentes}AvenirLTStd-Medium.otf')
+    propticks = font_manager.FontProperties(
+        fname=f'{ruta_fuentes}AvenirLTStd-Book.otf', size=18)
+    proplatlon = font_manager.FontProperties(
+        fname=f'{ruta_fuentes}AvenirLTStd-Book.otf', size=18)
+    proplegend = font_manager.FontProperties(
+        fname=f'{ruta_fuentes}AvenirLTStd-Book.otf', size=16)
+    proptitlebar = font_manager.FontProperties(
+        fname=f'{ruta_fuentes}AvenirLTStd-Book.otf', size=18)
+    return proptitle, propticks, proplatlon, proplegend, proptitlebar
